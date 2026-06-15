9e9adface83c4d06c7fc3c03b32deb7316d54fea
Walter Lopez <wjf.lopez@yavirac.edu.ec>
2026-06-15 08:38:53 -0500

    Crear controlador de autenticacion y peticion de registro

--- src/modules/auth/dto/login-auth.dto.ts
+++ src/modules/auth/dto/login-auth.dto.ts
@@ -1,6 +1,13 @@
-import { User } from "../interfaces/user.interface";
+import { IsEmail, IsNotEmpty, MaxLength, MinLength} from "class-validator";
+import { User } from "./interfaces/user.interface";
 
 export class LoginAuthDto implements User {
-  email!: string;
-  password!: string;
-}
+@IsEmail()
+@IsNotEmpty()
+email!: string;
+@MinLength(6)
+@MaxLength(25)
+@IsNotEmpty()
+password!: string;
+
+ }
