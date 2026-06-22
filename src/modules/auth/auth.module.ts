1c480d2d935cb9652c46a0bdbcf3ac53b2d9b426
Walter <wjf.lopez@yavirac.edu.ec>
2026-06-21 20:57:49 -0500

    Agrega JWT Guard y correcciones de autenticación

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  controllers: [AuthController]
+  imports: [
+    JwtModule.register({
+      secret: 'MI_CODIGO_SECRETO',
+      signOptions: {
+        expiresIn: '60S',
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
